7d59cf7890ba0d274f34c6b8572e084eb94cbd3a
lusianasavara <lusianasavara16@gmail.com>
2025-12-05 01:35:26 +0700

    add: fitur navbar, login, register

---
+++ src/MainLayout/MainLayout.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Route, Routes } from "react-router"
+import Navbar from "../Components/Navbar/Navbar.jsx"
+import Home from "../Pages/Home/Home.jsx"
+import Auth from "../Pages/Auth.jsx"
+import Login from "../Pages/Auth/Login/Login.jsx"
+import Register from "../Pages/Auth/Register/Register.jsx"
+import Error from "../Pages/Error/Error.jsx"
+import AuthCheck from "../AuthCheck/AuthCheck.jsx"
+
+
+const MainLayout = () => {
+  return (
+  
+  <BrowserRouter>
+    {/* Navbar */}
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={
+        <AuthCheck>
+          <Home/>
+        </AuthCheck>
+        }/> 
+      <Route path="/auth" element={<Auth />}/>
+          <Route path="/auth/login" element={<Login/>}/>
+          <Route path="/auth/register" element={<Register/>}/>
+      {/* not found route */}
+      <Route path="*" element={<Error/>} />
+    </Routes>
+    </BrowserRouter>
+
+  );
+};
+
+export default MainLayout
