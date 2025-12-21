5bdca3525a2f9179012ad7a6b1cb2b8db94e45b6
Arni <your_email@example.com>
2025-12-21 20:44:56 +0700

    add: product client

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,23 +1,19 @@
 import Banner from "../../Components/Banner/Banner.jsx"
 import Brand from "../../Components/Brand/Brand.jsx"
+import Client from "../../Components/Client/Client.jsx"
 import Features from "../../Components/Features/Features.jsx"
+import Product from "../../Components/Product/Product.jsx"
 import Recent from "../../Components/Recent/Recent.jsx"
-import Delivery from "../../Components/Delivery/Delivery.jsx"
-import Categories from "../../Components/Categories/Categories.jsx"
+
 
 
 const Home = () => {
   return (
     <div>
-      
+
       {/* banner component */}
       <div className="w-full min-h-[850px] bg-[#f0f2f3] flex items-center justify-center">
         <Banner></Banner>
-      </div>
-
-      {/* delivery component */}
-      <div className="delivery_component w-full min-h-[#150px]">
-        <Delivery></Delivery>
       </div>
 
       {/* brand component */}
@@ -30,13 +26,13 @@ const Home = () => {
         <Features></Features>
       </div>
 
-      {/* categories component */}
-      <div className="w-full flex items-center justify-center  pt-[80px] mb-[80px]">
-        <Categories></Categories>
+      {/* product component  */}
+      <div className="w-full flex items-center justify-center pb-[80px]">
+        <Product></Product>
       </div>
 
-      {/* client component */}
-      <div className="w-full flex items-center justify-center bg-[#f0f2f3] min-h-[589px]">
+      {/* client say component  */}
+      <div className="w-full flex items-center justify-center bg-[#f0f2f3] min-h-[589px] pb-[80px] pt-[80px]">
         <Client></Client>
       </div>
 
